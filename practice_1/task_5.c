5b6767fd8ac263a5b3559b0c39a28545be799dd8
Spravut <elagigzena@gmail.com>
2025-02-12 09:27:43 +0300

    5-th modify && 6-th

--- practice_1/task_5.c
+++ practice_1/task_5.c
@@ -1,16 +1,21 @@
 #include <stdio.h>
+#include <math.h>
 
 int main() {
     double r, f, dif;
     printf("Введите расчетную и фактическую скорости (вещественные значения). Введите значения через пробел: ");
     scanf("%lf %lf", &r, &f);
+
     dif = f - r;
-    if (dif < 0) {
-        dif = -dif;
+    double rounded_dif = round(dif * 10) / 10;
+    if (rounded_dif < 0) {
+        rounded_dif = -rounded_dif;
     }
-    if (dif > 0.1) {
-        printf("Орбита нестабильна на %.2lf м/с :<\n", dif);
+    if (rounded_dif > 0.1) {
+        printf("Орбита нестабильна на %lf м/с :<\n", rounded_dif);
     } else {
         printf("Орбита стабильна, все хорошо :>\n");
     }
+    return 0;   
 }
+// компилируем файл обязательно с флагом -lm
